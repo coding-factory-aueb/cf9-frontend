c428bdfc6de3fa08a452924d2d324db6f28fcb18
mkarampatsis <marka@central.ntua.gr>
2026-05-05 21:00:24 +0300

    feat: add google login

--- src/app/shared/services/google.service.ts
+++ src/app/shared/services/google.service.ts
@@ -6,7 +6,40 @@ import { jwtDecode } from 'jwt-decode';
 import { environment } from '../../../environments/environment';
 import { IGoogleUser } from '../interfaces/google-user.interface';
 
+const API_AUTH_URL = `${environment.apiURL}/api/auth/google-auth`;
+declare const google:any;
+
 @Injectable({
   providedIn: 'root',
 })
-export class GoogleService {}
+export class GoogleService {
+  http: HttpClient = inject(HttpClient);
+  router = inject(Router);
+
+  googleUser = signal<IGoogleUser | null>(null)
+
+  initializeGoogleSignIn(){
+    google.accounts.id.initialize({
+      client_id: GoogleClientId,
+      callback: (response: any)=>this.handleCredentials(response)
+    });
+    google.accounts.id.prompt();
+    return google;
+  }
+
+  handleCredentials(response:any) {
+    console.log(response);
+    const idToken = response.credentials;
+    this.loginGoogle(idToken)
+      .subscribe({
+        next: (res) =>{
+          console.log(res)
+        },
+        error: (err) => console.log("Problem in google login", err)
+      })
+  }
+
+  loginGoogle(token:string){
+    return this.http.post<{token:string}>(`${API_AUTH_URL}`, {token})
+  }
+}
