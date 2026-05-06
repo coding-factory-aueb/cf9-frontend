5329f7fb1fa153b0528eb24819cf903639af9077
mkarampatsis <marka@central.ntua.gr>
2026-05-06 20:05:28 +0300

    feat: add create user

--- src/app/shared/services/google.service.ts
+++ src/app/shared/services/google.service.ts
@@ -5,6 +5,7 @@ import { Router } from '@angular/router';
 import { jwtDecode } from 'jwt-decode';
 import { environment } from '../../../environments/environment';
 import { IGoogleUser } from '../interfaces/google-user.interface';
+import { UserService } from './user.service';
 
 const API_AUTH_URL = `${environment.apiURL}/api/auth/google-auth`;
 declare const google:any;
@@ -15,31 +16,41 @@ declare const google:any;
 export class GoogleService {
   http: HttpClient = inject(HttpClient);
   router = inject(Router);
+  userService = inject(UserService)
 
   googleUser = signal<IGoogleUser | null>(null)
 
   initializeGoogleSignIn(){
     google.accounts.id.initialize({
       client_id: GoogleClientId,
-      callback: (response: any)=>this.handleCredentials(response)
+      callback: (response: any) => this.handleCredentials(response)
     });
     google.accounts.id.prompt();
     return google;
   }
 
   handleCredentials(response:any) {
-    console.log(response);
-    const idToken = response.credentials;
+    // console.log("1>>>>", response);
+    const idToken = response.credential;
     this.loginGoogle(idToken)
       .subscribe({
         next: (res) =>{
-          console.log(res)
+          // console.log(res)
+          const decodedToken= jwtDecode(res.token) as IGoogleUser;
+          console.log(decodedToken);
+          localStorage.setItem('google-access-token', res.token);
+          this.userService.user.set({
+            username: decodedToken.name,
+            email: decodedToken.email,
+            roles: decodedToken.roles
+          })
         },
         error: (err) => console.log("Problem in google login", err)
       })
   }
 
   loginGoogle(token:string){
+    // console.log("TOKEN", token)
     return this.http.post<{token:string}>(`${API_AUTH_URL}`, {token})
   }
 }
